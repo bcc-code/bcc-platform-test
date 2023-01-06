75e39b5addaf03fd355ed786b410f0090132557d
Adelin Niculae <43118261+adelinn@users.noreply.github.com>
2023-01-06 17:16:34 +0100

    Update main.tf

--- azapi/main.tf
+++ azapi/main.tf
@@ -4,8 +4,8 @@ terraform {
     azapi = {
       source  = "bcc-code/azapi"
     }
-    azurerm = {
-      source  = "hashicorp/azurerm"
+    azuread = {
+      source  = "hashicorp/azuread"
     }
   }
 }
@@ -21,7 +21,7 @@ data "azuread_client_config" "current" {}
 resource "azapi_resource" "example107" {
   type      = "Microsoft.Resources/resourceGroups@2021-04-01"
   name      = "registry107"
-  parent_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  parent_id = "/subscriptions/${data.azuread_client_config.current.subscription_id}"
   location = "West Europe"
   tags = {
     "key" = "value2"
